7000afc5243784138b05318c441d413249f55d5e
JR-Camacho <josecamachoc0303@gmailcom>
2022-08-26 21:25:12 -0400

    Se modifica la ruta de la API

--- src/app/cliente.service.ts
+++ src/app/cliente.service.ts
@@ -12,18 +12,18 @@ export class ClienteService {
   clientes:Cliente[] = [];
 
   getClientes(query:string = ''){
-    return this.http.get('http://127.0.0.1:8000/clientes', {params: {buscar:query}});
+    return this.http.get('https://clientesdemo.herokuapp.com/clientes', {params: {buscar:query}});
   }
 
   setCliente(cliente:Cliente){
-    return this.http.post('http://127.0.0.1:8000/clientes', cliente);
+    return this.http.post('https://clientesdemo.herokuapp.com/clientes', cliente);
   }
 
   updateCliente(cliente:Cliente){
-    return this.http.put(`http://127.0.0.1:8000/clientes/${cliente.id}`, cliente);
+    return this.http.put(`https://clientesdemo.herokuapp.com/clientes/${cliente.id}`, cliente);
   }
 
   deleteCliente(id:number){
-    return this.http.delete(`http://127.0.0.1:8000/clientes/${id}`);
+    return this.http.delete(`https://clientesdemo.herokuapp.com/clientes/${id}`);
   }
 }
